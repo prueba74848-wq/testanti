8cc3e0f57e1b1c6bf6cf9d785beb92f09348b6ac
prueba74848-wq <prueba74848@gmail.com>
2026-09-02 22:04:47 +0200

    Update credits.jsx

--- components/credits.jsx
+++ components/credits.jsx
@@ -4,16 +4,11 @@ import { React, url } from "@vendetta/metro/common";
 import { Forms, General } from "@vendetta/ui/components";
 import { getAssetIDByName } from "@vendetta/ui/assets"
 import { findByStoreName } from "@vendetta/metro";
-
-const UserStore = findByStoreName("UserStore");
-
 const { ScrollView, View, Image } = General;
 const { FormArrow, FormRow, FormSection, FormDivider } = Forms;
-
 const devs = [
 	{ name: 'Angel', role: 'Author & Maintainer', uuid: "692632336961110087" }
 ];
-
 const qa   = [
 	{ name: 'Moodle', role: 'Quality Assurance', uuid: "807170846497570848" },
 	{ name: 'Rairof', role: 'Quality Assurance', uuid: "923212189123346483" },
@@ -34,29 +29,21 @@ const links = [
 		url: 'https://ko-fi.com/angel_wolf',
 	},
 ];
-
-
 export default function CreditsPage() {
 	useProxy(storage)
-
+	const UserStore = findByStoreName("UserStore");
 	const open = (uri) => url.openURL(uri).catch(() => {});
-
-	const getUser = id => UserStore?.getUser(id) || Object.values(UserStore?.getUsers()).find(u => u.id === id) || null;
-
+	const getUser = id => UserStore?.getUser(id) || Object.values(UserStore?.getUsers() || {}).find(u => u.id === id) || null;
 	const getUserPng = id => {
 		const u = getUser(id);
 		return u?.getAvatarURL?.()?.replace('webp', 'png') || null
 	};
-
 	const box = u => (<Image source={{ uri: u }} style={{ width: 40, height: 40, borderRadius: 20 }} />);
-
 	return (<>
 		<ScrollView>
 			<FormSection title="Developers">
 				{devs.map((p, i) => {
-
 					const avatarUri = getUserPng(p?.uuid)
-
 					return (<FormRow
 						key={i}
 						label={p.name}
@@ -67,9 +54,7 @@ export default function CreditsPage() {
 			</FormSection>
 			<FormSection title="Testers">
 				{qa.map((p, i) => {
-
 					const avatarUri = getUserPng(p?.uuid)
-
 					return (<FormRow
 						key={i}
 						label={p.name}
@@ -79,18 +64,14 @@ export default function CreditsPage() {
 				})}
 			</FormSection>
 			<FormDivider />
-
 			<FormSection title="Support & Source">
 				<View style={{ margin: 50 }}>
 					{links.map((l, i) => {
-
 						let finalIcon = l.icon ? (
 							l.icon?.startsWith("https") ? 
 							(<Image source={{ uri: l.icon }} style={{ width: 120, height: 40 }} />) :
 							(<FormRow.Icon source={getAssetIDByName(l.icon)}/>)
 						) :	null;
-
-
 						return (
 							<FormRow
 								key={i}
@@ -104,7 +85,6 @@ export default function CreditsPage() {
 			</View>
 			</FormSection>
 			<FormDivider />
-
 			{/* extra bottom pad so last row isn’t hidden by inset */}
 			<View style={{ height: 40 }} />
 		</ScrollView>
