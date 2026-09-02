dac9e0f62013c49592e35096d435080768229cdb
prueba74848-wq <prueba74848@gmail.com>
2026-09-02 21:50:49 +0200

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -2,15 +2,12 @@ import fluxDispatchPatch from "./patches/flux_dispatch";
 import selfEditPatch from "./patches/self_edit";
 import actionsheet from "./patches/actionsheet";
 import SettingPage from "./Settings";
-import { fetchDB, selfDelete } from "~lib/func/bl";
 
 export const regexEscaper = string => string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
 export let isEnabled = false;
 
 const deletedMessageArray = new Map();
 let patches = [];
-
-const database = "https://angelix1.github.io/static_list/antied/list.json";
 
 export default {
 	onLoad: async () => {
@@ -31,13 +28,6 @@ export default {
 					}
 				})
 				.filter(Boolean);
-
-			try {
-				const datas = await fetchDB(database);
-				selfDelete(datas, 15); // 15 sec
-			} catch (e) {
-				console.error("[ANTIED Zero] fetchDB/selfDelete failed\n", e);
-			}
 
 			isEnabled = true;
 		} catch (e) {
